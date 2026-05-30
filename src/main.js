1800dfc704938312faed467aac51b0deacbf110a
@guo_yaqun <2805007807@qq.com>
2026-05-30 15:00:55 +0800

    feat: 项目初始化

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createSSRApp } from 'vue'
 import * as Pinia from 'pinia'
 import App from './App.vue'
-import './permission.js'
 
 export function createApp() {
     const app = createSSRApp(App)
